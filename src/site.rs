9abf6f4b15cfec214c76385be9f7e62867416fcf
WCollier <me@williamcollier.me.uk>
2023-11-05 21:20:38 +0000

    Use route file path when generating the site

--- src/site.rs
+++ src/site.rs
@@ -3,9 +3,11 @@ use maud::Markup;
 use args::Args;
 use page::{Page, PageKind, BlogPost};
 use templates::{master_template, navbar_items};
+use navbar_link::NavbarLink;
 
 pub(crate) struct Site<'a> {
     pub(crate) pages: &'a [Page],
+    pub(crate) navbar_links: &'a [NavbarLink],
     pub(crate) args: Args,
 }
 
@@ -22,6 +24,7 @@ impl Site<'_> {
         fs::create_dir_all(format!("{}/", Self::BLOG_BUILD_PATH))?;
 
         fs::create_dir_all(format!("{}/posts", Self::BLOG_BUILD_PATH))?;
+
         fs::create_dir_all(format!("{}/static", Self::BLOG_BUILD_PATH))?;
 
         for page in self.pages {
@@ -46,12 +49,20 @@ impl Site<'_> {
     }
 
     fn generate(&self, navbar_items: &Markup, page: Page) -> std::io::Result<()> {
-        let route = format!("{}{}.html", Self::BLOG_BUILD_PATH, page.route);
         let html = master_template(navbar_items, page.title, page.template(self)).into_string();
+        let route = page.route.file_path();
 
-        println!("Written {route}");
+        if let Some(parent) = route.parent() {
+            let route = format!("{}{}", Self::BLOG_BUILD_PATH, route.display());
 
-        fs::write(route, html)
+            println!("Written {}", route);
+
+            fs::create_dir_all(format!("{}{}", Self::BLOG_BUILD_PATH, parent.display()))?;
+
+            fs::write(route, html)?;
+        }
+
+        Ok(())
     }
 }
 
