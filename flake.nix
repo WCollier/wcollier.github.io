03bafebee527bd7c1a6d28c6bbe83f48ecf2120d
WCollier <me@williamcollier.me.uk>
2025-06-08 16:02:27 +0100

    Remove flake-utils

--- flake.nix
+++ flake.nix
@@ -3,45 +3,66 @@
 
   inputs = {
     nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
-    flake-utils.url = "github:numtide/flake-utils";
     rust-overlay = {
       url = "github:oxalica/rust-overlay";
       inputs.nixpkgs.follows = "nixpkgs";
     };
   };
 
-  outputs = { self, nixpkgs, flake-utils, rust-overlay }:
-    flake-utils.lib.eachDefaultSystem (system:
-      let 
-        overlays = [ (import rust-overlay) ];
-        pkgs = import nixpkgs { inherit system overlays; };
-        rustPlatform = pkgs.makeRustPlatform {
-          cargo = pkgs.rust-bin.stable.latest.default;
-          rustc = pkgs.rust-bin.stable.latest.default;
-        };
-        cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
-        site-generator = rustPlatform.buildRustPackage {
-          pname = cargoToml.package.name;
-          version = cargoToml.package.version;
-          src = ./.;
-          cargoLock.lockFile = ./Cargo.lock;
-        };
-        host = pkgs.writeShellApplication {
-          name = "host";
-          runtimeInputs = [ pkgs.simple-http-server site-generator ];
-          text = ''
-            ${site-generator}/bin/site-generator
-            cd _site
-            simple-http-server -i
-          '';
-        };
-      in
-      {
-        devShells.default = with pkgs; mkShell {
-          buildInputs = [ rust-bin.stable.latest.default ];
-        };
-
-        apps = {
+  outputs =
+    {
+      nixpkgs,
+      rust-overlay,
+      ...
+    }:
+    let
+      overlays = [ (import rust-overlay) ];
+      cargoToml = builtins.fromTOML (builtins.readFile ./Cargo.toml);
+      forAllSystems =
+        f:
+        nixpkgs.lib.genAttrs [ "x86_64-linux" "aarch64-linux" "x86_64-darwin" "aarch64-darwin" ] (
+          system:
+          f {
+            pkgs = import nixpkgs { inherit system overlays; };
+          }
+        );
+    in
+    {
+      devShells = forAllSystems (
+        { pkgs }:
+        {
+          default = pkgs.mkShell {
+            buildInputs = [ pkgs.rust-bin.stable.latest.default ];
+          };
+        }
+      );
+      apps = forAllSystems (
+        { pkgs }:
+        let
+          rustPlatform = pkgs.makeRustPlatform {
+            cargo = pkgs.rust-bin.stable.latest.default;
+            rustc = pkgs.rust-bin.stable.latest.default;
+          };
+          site-generator = rustPlatform.buildRustPackage {
+            pname = cargoToml.package.name;
+            version = cargoToml.package.version;
+            src = ./.;
+            cargoLock.lockFile = ./Cargo.lock;
+          };
+          host = pkgs.writeShellApplication {
+            name = "host";
+            runtimeInputs = [
+              pkgs.simple-http-server
+              site-generator
+            ];
+            text = ''
+              ${site-generator}/bin/site-generator
+              cd _site
+              simple-http-server -i
+            '';
+          };
+        in
+        {
           default = {
             type = "app";
             program = "${host}/bin/host";
@@ -51,7 +72,7 @@
             type = "app";
             program = "${site-generator}/bin/site-generator";
           };
-        };
-      }
-    );
+        }
+      );
+    };
 }
